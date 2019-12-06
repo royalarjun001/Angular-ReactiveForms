0fcb69ef97ae0890492007e55bad530d7f5048f4
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-12-06 06:07:11 +0530

    Create From model using Form group and form control

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { FormGroup, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -9,15 +8,32 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  // This is Root FormGroup and it define our form model
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() { }
 
   ngOnInit() {
+
+    // NOTES
+    // This structure is form model and it tracks the value and state of this
+    // form. it is not the data model, this should match with the input defined
+    // in the HTML. The data model here is
+    //   e.g  customer = new Customer();
+    // and data model defines the data which transfer through and back from the
+    // server.
+
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+   console.log(this.customerForm);
+   console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
