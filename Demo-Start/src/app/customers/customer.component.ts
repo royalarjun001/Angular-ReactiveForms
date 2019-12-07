f9ab5733f774afc1c038889a04924973ce64f7fa
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-12-07 07:24:13 +0530

    Implement runtime or optional validator in angular

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -35,6 +35,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -49,7 +51,19 @@ export class CustomerComponent implements OnInit {
       firstName: 'Arjun Kumar',
       lastName: 'verma',
       email: 'xyz@google.com',
+      phone: '',
+      notification: 'email',
       sendCatalog: false
     });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
